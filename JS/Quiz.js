0f40ee2e11204fda9e88a6f98083ff70683aa9b6
TanmayGadge <tanmaygadge9273@gmail.com>
2021-04-08 21:49:54 +0530

    myQuiz

---
+++ JS/Quiz.js
@@ -0,0 +1,36 @@
+class Quiz {
+    constructor() {
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+
+    async start(){
+        if (gameState === 0) {
+            contestant = new contestant();
+            var contestantCountRef = await database.ref('contestantCount').once("value");
+            if (contestantCountRef.exists()) {
+                contestanCount = contestanCountRef.val();
+                contestan.getCount();
+            }
+            form = new Form()
+            form.display();
+        }
+        contestan1 = createSprite(200, 500);
+        contestan1.addImage("contestan1", contestan_img);
+
+        contestan2 = createSprite(800, 500);
+        contestan2.addImage("contestan2", contestan_img);
+        contestans = [contestan1, contestan2];
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,15 @@
+var database;
+var gameState = 0, contestantCount, database;
+var quiz, question, contestant;
+
+function setup() {
+  createCanvas(850, 400);
+  database = firebase.database();
+}
+
+
+function draw() {
+  background(0);
+
+
+}
